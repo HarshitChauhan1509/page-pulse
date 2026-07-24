f3de3316708853dc8e7926a29e7f4e7e5fb1454b
HarshitChauhan15 <harshitchauhan1509@gmail.com>
2026-07-24 22:55:37 +0530

    Create requestId.js

---
+++ src/middlewares/requestId.js
@@ -0,0 +1,11 @@
+import { randomUUID } from "crypto";
+
+export const requestId = (req, res, next) => {
+    const id = randomUUID();
+
+    req.requestId = id;
+
+    res.setHeader("X-Request-ID", id);
+
+    next();
+};
