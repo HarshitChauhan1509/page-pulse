9c153a73c3ff525a9e7eb0e83a8bad8859591b0e
HarshitChauhan15 <harshitchauhan1509@gmail.com>
2026-07-26 17:31:06 +0530

    Create Footer.jsx

---
+++ client/components/Footer.jsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <footer className="mt-16 py-8 text-center text-gray-600">
+
+      Built for{" "}
+
+      <a
+        href="https://digitalheroesco.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-blue-600 hover:underline font-semibold"
+      >
+        Digital Heroes Training Task
+      </a>
+
+    </footer>
+  );
+}
